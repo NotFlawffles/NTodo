653a2929b2a83bcb0ab01868513e06fbc75dfa7d
root <hassenjasim3@gmail.com>
2022-07-03 20:05:39 +0300

    updating

--- src/main.c
+++ src/main.c
@@ -18,9 +18,9 @@ int main(int argc, char **argv) {
     else if (strcmp(argv[1], "remove")  == 0)    removes(vm, argc, argv);
     else if (strcmp(argv[1], "check")   == 0)    check(vm, argc, argv);
     else if (strcmp(argv[1], "uncheck") == 0)    uncheck(vm, argc, argv);
-    else if (strcmp(argv[1], "dest")    == 0)    dest(vm, argc,  argv);
+    else if (strcmp(argv[1], "dest")    == 0)    dest(vm);
     else if (strcmp(argv[1], "list")    == 0)    list(vm, argc, argv);
-    else if (strcmp(argv[1], "update")  == 0)    update(vm, argc, argv);
+    else if (strcmp(argv[1], "update")  == 0)    update(vm);
     else                                         unknownCommand(argv[1]);
     return 0;
 }
